2d79bce246f3728d20363ab70c08c92cbe69da55
narriteration <narriteration@gmail.com>
2017-04-04 15:40:18 -0700

    updated nested components, added AllCityPosts component with CityPost nested inside

---
+++ src/components/CityPost.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import '../css/App.css';
+// import 'react-materialize';
+
+class CityPost extends Component {
+  render() {
+    return (
+        <div className="cityPost">
+            <h1>title post</h1>
+            <h3>title author<h3>
+            <p>post content: Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    );
+  }
+}
+
+export default CityPost;
--- src/components/UserCityFeed.js
+++ src/components/UserCityFeed.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 import 'react-materialize';
-import CityInfo from './CityInfo.js'
+import NavBarDynamic from './NavBarDynamic';
+import CityInfo from './CityInfo.js';
+import AllCityPosts from './AllCityPosts';
+import NewPostForm from './NewPostForm';
 
 class SignInForm extends Component {
 
-TODO: Styles
-* connect to materialize
+// TODO: Styles
+// * write specific styles to make all three imported components pretty
+//
+// TODO: View logic
+// * render this component after user clicks submit on SignInForm.js
 
 
   render() {
     return (
       <div className="userCityFeed">
-          <CityInfo />
-          <CityPost />
+          <NavBarDynamic /> // renders nav bar at top with appropriate buttons and search bar
+          <CityInfo /> // renders information specific to THIS city only
+          <AllCityPosts /> // renders all CityPosts that ANY user has published about this city
+          <NewPostForm /> // renders form to create new post about THIS city, on submit will close and add this post to CityPosts
       </div>
     );
   }
