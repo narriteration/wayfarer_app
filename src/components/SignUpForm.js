79dd49d6afcd40e94011b8b4a333e62391264eba
narriteration <narriteration@gmail.com>
2017-04-04 12:37:47 -0700

    added component: NewUserForm and embedded it into SignIn and SignUp

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,59 +1,16 @@
 import React, { Component } from 'react';
-import '../css/App.css';
-import 'react-materialize';
+// import '../css/App.css';
+// import 'react-materialize';
+import NewUserForm from './NewUserForm.js'
 
 
 
 class SignUpForm extends Component {
-  render() { 
+  render() {
     return (
-      <div className="signUpForm">
 
-      // Option 1: Plain HTML form, needs styling and className + action
-        <h1>Sign up form:</h1>
-        <form className="######" action="######" method="post">
-          <input id="sign-up-email" type="text" name="email" value="Enter email"/>
-          <input id="sign-up-password" type="text" name="password" value="Enter password"/>
-          <button type="submit" name="sign-up-form-button">Create New User</button>
-        </form>
+      <NewUserForm />
 
-
-        // Option 2: Materialize template for form. Can grab conditional styling from website: http://materializecss.com/forms.html
-        <div class="row">
-           <form class="col s12">
-             <div class="row">
-               <div class="input-field col s6">
-                 <input placeholder="Your first name, example: Alix" id="first_name" type="text" class="validate"/>
-                 <label for="first_name">First Name</label>
-               </div>
-               <div class="input-field col s6">
-                 <input placeholder="Your last name, example: Robinson" id="last_name" type="text" class="validate"/>
-                 <label for="last_name">Last Name</label>
-               </div>
-             </div>
-             <div class="row">
-               <div class="input-field col s12">
-                 <input id="sign-up-password" type="password" class="validate"/>
-                 <label for="password">Password</label>
-               </div>
-             </div>
-             <div class="row">
-               <div class="input-field col s12">
-                 <input id="email" type="email" class="validate"/>
-                 <label for="email">Email</label>
-               </div>
-             </div>
-             <div class="row">
-                <div class="button col s3">
-                  <button type="submit" name="sign-up-form-button">Create New User</button>
-                </div>
-             </div>
-           </form>
-         </div>
-        //  END Materialze template for form
-
-
-      </div>
     );
   }
 }
