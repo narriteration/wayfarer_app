c77fba31a99ed8f56d612eb3a41e326b1ca5fbd2
narriteration <narriteration@gmail.com>
2017-04-03 21:59:33 -0700

    changed name from LogInForm to SignInForm; updated SignUpForm.js button and deleted some unused code

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -23,23 +23,17 @@ class SignUpForm extends Component {
            <form class="col s12">
              <div class="row">
                <div class="input-field col s6">
-                 <input placeholder="Placeholder" id="first_name" type="text" class="validate"/>
+                 <input placeholder="Your first name, example: Alix" id="first_name" type="text" class="validate"/>
                  <label for="first_name">First Name</label>
                </div>
                <div class="input-field col s6">
-                 <input id="last_name" type="text" class="validate"/>
+                 <input placeholder="Your last name, example: Robinson" id="last_name" type="text" class="validate"/>
                  <label for="last_name">Last Name</label>
                </div>
              </div>
              <div class="row">
                <div class="input-field col s12">
-                 <input disabled value="I am not editable" id="disabled" type="text" class="validate"/>
-                 <label for="disabled">Disabled</label>
-               </div>
-             </div>
-             <div class="row">
-               <div class="input-field col s12">
-                 <input id="password" type="password" class="validate"/>
+                 <input id="sign-up-password" type="password" class="validate"/>
                  <label for="password">Password</label>
                </div>
              </div>
@@ -50,13 +44,9 @@ class SignUpForm extends Component {
                </div>
              </div>
              <div class="row">
-               <div class="col s12">
-                 This is an inline input field:
-                 <div class="input-field inline">
-                   <input id="email" type="email" class="validate"/>
-                   <label for="email" data-error="wrong" data-success="right">Email</label>
-                 </div>
-               </div>
+                <div class="button col s3">
+                  <button type="submit" name="sign-up-form-button">Create New User</button>
+                </div>
              </div>
            </form>
          </div>
