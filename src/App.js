63e0ac36490f4c6935833337caac30146724ef9d
namelessprofit <posadaarchy@gmail.com>
2017-04-03 23:45:34 -0700

    finshed splash page and added fonts styles. also created the for for new posts and added logic. data is binding to dom but not being stored in db yet.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import 'react-materialize';
-<<<<<<< HEAD
-=======
+import NewPostForm from './components/NewPostForm';
 
-
->>>>>>> 6de170901002fb9c4a534c32c37874931e8ee590
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header" id="header">
-          <div className="Title">
-<<<<<<< HEAD
-              <h1>Welcome to Wayferer!</h1>
-              <p>A travel community for users to share city-specific tips about their favorite locations around the world.</p>
-                <button type="button" className="btn btn-primary">Log In</button>
-                <button type="button" className="btn btn-primary">Sign Up</button>
-        </div>
-        </div>
+ render() {
+   return (
+     <div className="App">
+       <div className="App-header" id="header">
+         <div className="Title">
+            <h1>Welcome to Wayfarer!</h1>
+            <p>A travel community for you to share city-specific tips about your favorite locations around the world.</p>
+            <a className="btn waves-effect waves-light">Log In</a>
+            <a className="btn waves-effect waves-light">Sign Up</a>
+            <NewPostForm />
 
-
-=======
- <h1>Welcome to Wayfarer!</h1>
- </div>
-        <a className="waves-effect waves-light btn-large">Log In</a>
-        <a className="waves-effect waves-light btn-large">Sign Up</a>
-        </div>
->>>>>>> 6de170901002fb9c4a534c32c37874931e8ee590
-      </div>
-
-    );
-  }
+       </div>
+     </div>
+   </div>
+   );
+ }
 }
 
 export default App;
