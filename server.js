34b4feac1fe973ca2091fdd81bf7a42b61df9ad4
narriteration <narriteration@gmail.com>
2017-04-05 13:49:28 -0700

    replaced routes in server.js from GH last version

--- server.js
+++ server.js
@@ -7,14 +7,8 @@ var express = require('express'),
     db = require('./models');
 
 
-
-
-
-//TODO: require model's files
-
 //Create instances
 var app = express();
-//var router = express.Router();
 
 
 //Set port
@@ -22,7 +16,6 @@ const port = process.env.API_PORT || 3001;
 
 //db config
 //ADD YOUR INFO HERE!
-//mongoose.connect
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({extended: true }));
@@ -30,23 +23,110 @@ app.use(bodyParser.json());
 
 //Prevent CORS errors
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('AccessControlAllowOrigin', '*');
+  res.setHeader('AccessControlAllowCredentials', 'true');
+  res.setHeader('AccessControlAllowMethods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('AccessControlAllowHeaders', 'AccessControlAllowHeaders, Origin,Accept, XRequestedWith, ContentType, AccessControlRequestMethod, AccessControlRequestHeaders');
 
   //Remove caching
-   res.setHeader('Cache-Control', 'no-cache');
+   res.setHeader('CacheControl', 'nocache');
    next();
  });
-
- //set route path
-app.route('/')
-  .get(function(req, res) {
-  res.send('Server.js is running');
-
+ //set route path		  //set route path
+ app.route('/')
+   .get(function(req, res) {
+   res.send('Server.js is running');
+  // .post(function(req, res) {
+  //     db.Location.
+  // })
 });
 
-app.listen(port, function() {
- console.log(`api running on port ${port}`);
+//creating user
+app.post('/api/user', function(req, res) {
+  db.User.create(req.body, function(err, user) {
+      if (err) { console.log('error', err); }
+      console.log(user);
+      res.json(user);
+    });
+})
+
+//delete user
+app.delete('/api/user/:userId', function(req, res){
+  db.User.findOneAndRemove({ _id: req.params.userId }, function(err, foundUser){
+    res.json(foundUser);
+  });
+})
+
+//edit user
+app.put('/api/user/:userId', function(req, res){
+    console.log(req.body)
+    db.User.findOneAndUpdate({_id: req.params.userId}, req.body, {new:true}, function(err, foundUser) {
+        if (err) {
+          console.log('got an error');
+        }
+      console.log(foundUser)
+        res.json(foundUser)
+    });
+  // at this point person is null.
 });
+
+//create new comments
+app.post('/api/comments', function(req, res) {
+  console.log(db.Comments)
+  db.Comments.create(req.body, function(err, comment){
+    if (err) {
+      console.log('got an error');
+    }
+    res.json(comment)
+  })
+});
+
+//delete a comment
+app.delete('/api/comments/:commentId', function(req, res){
+  db.Comments.findOneAndRemove({ _id: req.params.commentId }, function(err, foundComment){
+    res.json(foundComment);
+  });
+})
+
+//edit comment
+app.put('/api/comments/:commentId', function(req, res){
+console.log(req.body)
+  db.Comments.findOneAndUpdate({_id: req.params.commentId}, req.body, {new:true}, function(err, foundComment) {
+        if (err) {
+          console.log('got an error');
+        }
+      console.log(foundComment)
+        res.json(foundComment)
+  });
+ });
+
+//get single post
+app.get('/api/comments/:commentId', function(req, res) {
+    db.Comments.findOne({_id: req.params.commentId}, function(err, foundComment) {
+        if (err) {
+            console.log('got an error');
+        }
+        res.json(foundComment)
+    })
+})
+
+//find all comments
+app.get('/api/comments', function(req, res) {
+    db.Comments.find({}, function(err, foundComments) {
+        if (err) {
+            console.log('got an error');
+        }
+        res.json(foundComments)
+    })
+})
+
+
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wayfarer_app");
+
+ app.listen(port, function() {
+  console.log(`api running on port ${port}`);
+ });
