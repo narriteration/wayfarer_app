e5fbefda7dfbffa36b4ec5247d85d86b7c34e546
ashleyvest5 <ashleyvest@Marys-MacBook-Pro.local>
2017-04-05 13:56:22 -0700

    renders city view upon creating account

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Splash from '../src/components/Splash';
 import App from './App';
 import SignInForm from '../src/components/SignInForm';
 import SignUpForm from '../src/components/SignUpForm';
+import UserCityFeed from '../src/components/UserCityFeed'
 
 import './css/index.css';
 
@@ -15,6 +16,7 @@ ReactDOM.render(
     <Route path='/wayfarer' component={App}></Route>
     <Route path='/sign-in' component={SignInForm}></Route>
     <Route path='/sign-up' component={SignUpForm}></Route>
+    <Route path='/user-city' component={UserCityFeed}></Route>
   </Router>,
   document.getElementById('main')
 );
