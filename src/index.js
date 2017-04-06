359c26c50b4d023400fa54dd843de4f6dc83aca7
narriteration <narriteration@gmail.com>
2017-04-05 20:38:42 -0700

    FIXED ERROR: PULL DOWN MASTER NOW YALL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Splash from '../src/components/Splash';
 import App from './App';
 import SignInForm from '../src/components/SignInForm';
 
-import SignUpForm from '../src/components/SignUpForm';
+import SignUp from '../src/components/SignUp';
 import CityFeed from '../src/components/CityFeed'
 
 
@@ -18,10 +18,10 @@ ReactDOM.render(
     <Route path='/wayfarer' component={App}></Route>
     <Route path='/sign-in' component={SignInForm}></Route>
 
-    <Route path='/sign-up' component={SignUpForm}></Route>
+    <Route path='/sign-up' component={SignUp}></Route>
     <Route path='/city-feed' component={CityFeed}></Route>
 
-  
+
   </Router>,
   document.getElementById('main')
 );
