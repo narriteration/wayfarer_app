f5bdfc7d98229a2704007c3e75a8c66555169386
namelessprofit <posadaarchy@gmail.com>
2017-04-05 18:56:51 -0700

    created new sign up form and satrted logic. still needs to append data to site.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Router, Route, browserHistory } from 'react-router';
 import Splash from '../src/components/Splash';
 import App from './App';
 import SignInForm from '../src/components/SignInForm';
-import SignUpForm from '../src/components/SignUpForm';
+import SignUp from '../src/components/SignUp';
 import UserCityFeed from '../src/components/UserCityFeed'
 
 import './css/index.css';
@@ -15,7 +15,7 @@ ReactDOM.render(
     <Route path='/' component={Splash}></Route>
     <Route path='/wayfarer' component={App}></Route>
     <Route path='/sign-in' component={SignInForm}></Route>
-    <Route path='/sign-up' component={SignUpForm}></Route>
+    <Route path='/sign-up' component={SignUp}></Route>
     <Route path='/user-city' component={UserCityFeed}></Route>
   </Router>,
   document.getElementById('main')
