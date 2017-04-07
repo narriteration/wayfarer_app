4c0002f9450b3df7716e54500f0f0a8b9b5b3e41
carlogropif <carlchampain@gmail.com>
2017-04-07 09:00:19 -0700

    final stuff and css

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -4,13 +4,20 @@ import '../css/UserProfile.css';
 export default class UserProfile extends Component {
     render(){
       return (
-            <div classNameName="container">
+            <div className="container">
                 <div className="userProfile">
                     <img align="left" className="fb-image-lg" src="http://lorempixel.com/850/280/nightlife/5/" alt="Profile image example"/>
                     <img align="left" className="fb-image-profile thumbnail" src="http://lorempixel.com/180/180/people/9/" alt="Profile image example"/>
                     <div className="fb-profile-text">
-                        <h1>Sasha Fierce</h1>
-                        <p>Girls just wanna go fun.</p>
+                        <h2>Sasha Fierce</h2>
+                        <h3>Paris, France</h3>
+                        <p> Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré....more</p>
+                        <h3>Milan, Italy</h3>
+                        <p>Rome, Italy’s capital, is a sprawling, cosmopolitan city with nearly 3,000 years of globally influential art, architecture and culture on display. Ancient ruins such as the Forum and the Colosseum evoke the power of the former Roman Empire. Vatican City, headquarters of the Roman Catholic Church, has St. Peter’s Basilica and the Vatican Museums, which house masterpieces such as Michelangelo’s Sistine Chapel frescoes...more</p>
+                        <h3>Mumbai, India</h3>
+                        <p>Mumbai (formerly called Bombay) is a densely populated city on India’s west coast. A financial center, it's India's largest city. On the Mumbai Harbour waterfront stands the iconic Gateway of India stone arch, built by the British Raj in 1924. Offshore, nearby Elephanta Island holds ancient cave temples dedicated to the Hindu god Shiva. The city's also famous as the heart of the Bollywood film industry...more</p>
+                        <h3>Barcelona, Spain</h3>
+                        <p>Barcelona, the cosmopolitan capital of Spain’s Catalonia region, is known for its art and architecture. The fantastical Sagrada Família church and other modernist landmarks designed by Antoni Gaudí dot the city. Museu Picasso and Fundació Joan Miró feature modern art by their namesakes. City history museum MUHBA, includes several Roman archaeological sites...more</p>
                     </div>
                 </div>
             </div>
